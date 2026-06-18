52fed948e6aecbd0a4dc951d06a95d358799e444
yuletsypabon <yuletsyp.pabonf@unac.edu.co>
2026-06-17 20:44:15 -0500

    feat: transform API response to daily records

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,6 +1,26 @@
+
 import requests
 
+url = "https://api.open-meteo.com/v1/forecast?latitude=7.125&longitude=-73.1189&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&past_days=7&forecast_days=1"
 
-response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=7.119&longitude=-73.122&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&past_days=7&forecast_days=0")
-print(response.status_code)
-print(response.json())
+
+def transform_weather_data(data):
+    results = []
+    for date, temp_max, temp_min,  rain in zip(
+            data["daily"]["time"], 
+            data["daily"]["temperature_2m_max"], 
+            data["daily"]["temperature_2m_min"], 
+            data["daily"]["precipitation_sum"]
+    ):
+        results.append({
+            "date": date,
+            "temp_max": temp_max,
+            "temp_min": temp_min,
+            "rain": rain
+        })
+    
+    return results
+response = requests.get(url)
+data = response.json()
+result = transform_weather_data(data)
+print(result)
